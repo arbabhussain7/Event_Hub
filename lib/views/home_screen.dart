f0240f087067b01787fb24ff76770d8e1f83cd3c
arbabhussain7 <arbabhussain414@gmail.com>
2025-04-06 19:31:51 +0500

    working in Home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:event_hub/constant/colors/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +8,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: AppColors.blueColor,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(12.r),
+                    bottomRight: Radius.circular(12.r))),
+          )
+        ],
       ),
     );
   }
